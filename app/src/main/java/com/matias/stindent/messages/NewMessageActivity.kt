fa6ddb0aae47a30ad26db3a34a618b59b8b90b1a
CaptainMomo <31258188+CaptainMomo@users.noreply.github.com>
2019-05-23 15:13:31 +0200

    Matching av BrukerFilter startet

--- app/src/main/java/com/matias/stindent/messages/NewMessageActivity.kt
+++ app/src/main/java/com/matias/stindent/messages/NewMessageActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -16,6 +17,8 @@ import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
 import kotlinx.android.synthetic.main.user_row_newmessage.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -32,8 +35,50 @@ class NewMessageActivity : AppCompatActivity() {
         val USER_KEY = "USER_KEY"
     }
 
+    var userInterests = ArrayList<String>()
+    var otherUserInterests = ArrayList<String>()
+
+    private fun getUserInterests(){
+        val user = FirebaseAuth.getInstance().uid ?: ""
+        if(user == null) return
+        val ref = FirebaseDatabase.getInstance().getReference("/user-interests/$user")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach {
+                    userInterests.add(it.toString())
+                    Log.d("UserFilter", "added " + it.toString() +"for user: " + user + "to userInterests which now contains " + userInterests.size)
+                }
+            }
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d("UserFilter", "didnt work man")
+
+            }
+        }
+        )
+    }
+    private fun getOtherUserInterests(){
+        val ref = FirebaseDatabase.getInstance().getReference("/user-interests")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach {
+                    otherUserInterests.add(it.toString())
+                    Log.d("UserFilter", "added " + it.toString() + "to other users interest")
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+    }
+
+
     private fun fetchUsers(){
         val ref = FirebaseDatabase.getInstance().getReference("/users")
+        //val refInterests = FirebaseDatabase.getInstance().getReference("/user-interests")
+        //getUserInterests()
+        //getOtherUserInterests()
+
         ref.addListenerForSingleValueEvent(object: ValueEventListener {
 
             override fun onDataChange(p0: DataSnapshot) {
@@ -44,6 +89,10 @@ class NewMessageActivity : AppCompatActivity() {
                     val user = it.getValue(User::class.java)
                     if (user != null){
                         adapter.add(UserItem(user))
+                        /*if (!Collections.disjoint(userInterests, otherUserInterests )){
+                            adapter.add(UserItem(user))
+                        }*/
+
                     }
                 }
 
